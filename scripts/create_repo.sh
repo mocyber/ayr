55d99c7d5ea90bea59c34c869fb5d4b68a4c3b34
Kirk <koelslaw@gmail.com>
2019-08-26 10:16:20 -0500

    Corrected directory parsing
    it wanted to throw it all in one.

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -7,20 +7,24 @@ REPOFILE="${REPOPATH}/${REPO_FOLDER}/local.repo"
 mkdir -p $REPOPATH
 rm $REPOFILE 2> /dev/null
 touch $REPOFILE
-
+ 
 echo "${REPOPATH}${REPO_FOLDER}"
 
-reposync -n -l --repoid=$REPO_ITEMS --download_path=/var/www/html/general_mirror/ --downloadcomps --download-metadata
-wait
+for i in $(echo $REPO_ITEMS | sed "s/,/ /g")
+do
+    reposync -n -l --repoid=$i --download_path=/var/www/html/general_mirror/ --downloadcomps --download-metadata
+    wait
 
-createrepo -v /var/www/html/general_mirror/$REPO_ITEMS -g comps.xml
+    createrepo -v /var/www/html/general_mirror/$i -g comps.xml
+    wait
+done
 
 for DIR in `find ${REPOPATH}/general_mirror/ -maxdepth 1 -mindepth 1 -type d`; do
     REPO_ITEM=$(basename $DIR)
     if [[ "${REPO_ITEMS}" =~ "${REPO_ITEM}" ]]; then
        echo -e "[${REPO_ITEM}]" >> $REPOFILE
        echo -e "name=${REPO_ITEM}" >> $REPOFILE
-       echo -e "baseurl=http://10.[state].10.19/general_mirror/${REPO_ITEM}/" >> $REPOFILE
+       echo -e "baseurl=http://[your.ip.or.url.goes.here]/general_mirror/${REPO_ITEM}/" >> $REPOFILE
        echo -e "enabled=1" >> $REPOFILE
        echo -e "gpgcheck=0" >> $REPOFILE
        echo -e "\n" >> $REPOFILE
