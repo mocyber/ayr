07a528c033a1b1a3b464927fc01471c238c4beba
dcode <derek@rocknsm.io>
2020-05-18 14:33:01 +0000

    iteration of changes to run on current nuc architecture

--- scripts/create_repo.sh
+++ scripts/create_repo.sh
@@ -1,7 +1,7 @@
 REPO_FOLDER=$1
 REPO_ITEMS=$2
 
-REPOPATH="/var/www/html/"
+REPOPATH="/srv/yum/"
 REPOFILE="${REPOPATH}/${REPO_FOLDER}/local.repo"
 
 mkdir -p $REPOPATH
@@ -12,10 +12,10 @@ echo "${REPOPATH}${REPO_FOLDER}"
 
 for i in $(echo $REPO_ITEMS | sed "s/,/ /g")
 do
-    reposync -n -l --repoid=$i --download_path=/var/www/html/general_mirror/ --downloadcomps --download-metadata
+    reposync -n -l --repoid=$i --download_path=/srv/yum/general_mirror/ --downloadcomps --download-metadata
     wait
 
-    createrepo -v /var/www/html/general_mirror/$i -g comps.xml
+    createrepo -v /srv/yum/general_mirror/$i -g comps.xml
     wait
 done
 
@@ -24,7 +24,7 @@ for DIR in `find ${REPOPATH}/general_mirror/ -maxdepth 1 -mindepth 1 -type d`; d
     if [[ "${REPO_ITEMS}" =~ "${REPO_ITEM}" ]]; then
        echo -e "[${REPO_ITEM}]" >> $REPOFILE
        echo -e "name=${REPO_ITEM}" >> $REPOFILE
-       echo -e "baseurl=http://[your.ip.or.url.goes.here]/general_mirror/${REPO_ITEM}/" >> $REPOFILE
+       echo -e "baseurl=http://repo.mocyber.lan/general_mirror/${REPO_ITEM}/" >> $REPOFILE
        echo -e "enabled=1" >> $REPOFILE
        echo -e "gpgcheck=0" >> $REPOFILE
        echo -e "\n" >> $REPOFILE
